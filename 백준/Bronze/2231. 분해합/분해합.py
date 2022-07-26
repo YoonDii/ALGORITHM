31c4f15b67bfc471860dc97a6943f8c3d33aa55e
Yoonji Kim <108647861+YoonDii@users.noreply.github.com>
2022-07-26 15:13:02 +0900

    [Bronze II] Title: 분해합, Time: 1308 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,10 +1,11 @@
 n = int(input())
 
 for i in range(n):            # 범위는 n 내에서
-    m = sum(map(int, str(i))) # i를 문자열로 변환하여 값을 더함.
-    result = i + m            # result는 i와 m을 합한값으로 초기화.  
+    m = sum(map(int, str(i))) # i를 문자열로 변환하여 값을 더함. 52 ==> 5+2
+    result = i + m            # result는 분해합으로 i와 m을 합한값.  
     if result == n:           # result값이 n과 같으면 i는 생성자라서 출력
         print(i)
         break
 else:                         # 생성자가 없으면 0을 출력
     print(0)
+    
