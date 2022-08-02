24ef9ac663dd8d0b0f05823143424ff2fd080d68
Yoonji Kim <108647861+YoonDii@users.noreply.github.com>
2022-08-02 14:12:04 +0900

    [Bronze III] Title: TGN, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/5063. TGN/TGN.py
@@ -0,0 +1,10 @@
+n = int(input())
+for _ in range(n):
+    r, e, c = map(int,input().split())
+
+    if (e - c) > r:
+        print("advertise")
+    elif (e - c) == r:
+        print("does not matter")
+    elif (e - c) < r :
+        print("do not advertise")
