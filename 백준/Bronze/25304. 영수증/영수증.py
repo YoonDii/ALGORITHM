4d6a0391c20779a97df4b40f197351b399474b9c
Yoonji Kim <108647861+YoonDii@users.noreply.github.com>
2022-08-11 14:11:03 +0900

    [Bronze V] Title: 영수증, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,11 @@
+x = int(input())
+n = int(input())
+
+for _ in range(n):
+    a, b = map(int,input().split())
+    #print(a,b)
+    x -= a * b
+if x == 0 :
+    print('Yes')
+else:
+    print('No')
