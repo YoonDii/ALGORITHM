e42d3719413c84e618c6fede1ba0d012b2bc77d6
YoonDii <108647861+YoonDii@users.noreply.github.com>
2022-12-26 15:01:58 +0900

    [level 0] Title: 가까운 수, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.py
@@ -0,0 +1,14 @@
+def solution(array, n):
+    box = []
+    array.sort()
+    for i in array:
+        box.append(abs(n-i))
+    answer = [array[box.index(min(box))]]
+    if len(answer) > 1:
+        return min(answer)
+    else:
+        return answer[0]
+        
+        
+            
+            
