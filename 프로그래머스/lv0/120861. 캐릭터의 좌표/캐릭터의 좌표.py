c483efa7dd61905e8107d6276a69e72b13c46f56
YoonDii <108647861+YoonDii@users.noreply.github.com>
2022-12-28 09:39:54 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -0,0 +1,14 @@
+def solution(keyinput, board):
+    col = board[0]
+    row = board[1]
+    result = [0, 0]
+    for i in keyinput:
+        if i == "left" and result[0]-1 >= -(col // 2):
+            result[0] -= 1
+        elif i == "right" and result[0]+1 <= (col // 2):
+            result[0] += 1
+        elif i == "up" and result[1]+1 <= (row // 2):
+            result[1] += 1
+        elif i == "down" and result[1]-1 >= -(row // 2):
+            result[1] -= 1
+    return result
