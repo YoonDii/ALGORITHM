dda8779dae8024e1da05042adef2043706c17499
YoonDii <108647861+YoonDii@users.noreply.github.com>
2023-01-04 17:14:00 +0900

    [D2] Title: 달팽이 숫자, Time: 95 ms, Memory: 43,252 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
@@ -1,23 +1,26 @@
-T = int(input())
-di = [0, 1, 0, -1]
-dj = [1, 0, -1, 0]
-for test_case in range(1, T + 1):
-    N = int(input())
-    arr = [[0] * N for _ in range(N)]
- 
-    i, j, cnt, dr = 0, 0, 1, 0 # 초기화
-    arr[i][j] = cnt
-    cnt += 1
- 
-    while cnt <= N*N:
-        ni, nj = i+di[dr], j+dj[dr]
-        if 0<=ni<N and 0<=nj<N and arr[ni][nj]==0:
-            i, j = ni, nj
-            arr[i][j]=cnt
-            cnt+=1
+
+t = int(input())
+# 우 하 좌 상
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+for tc in range(1, t + 1):
+    n = int(input())
+    arr = [[0] * n for _ in range(n)]
+    # print(arr)[[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    x, y, cnt, dr = 0, 0, 1, 0  # 초기값 // cnt가 1인 이유는 1부터 시작해서
+    arr[x][y] = cnt
+    cnt += 1  # 숫자가 증가하니까 cnt도 증가
+
+    while cnt <= n * n:
+        nx, ny = x + dx[dr], y + dy[dr]  # 다음좌표는 현재좌표+방향
+
+        if 0 <= nx < n and 0 <= ny < n and arr[nx][ny] == 0:
+            # 조건에 맞으면 다음좌표로 넘어가기
+            x, y = nx, ny
+            arr[x][y] = cnt
+            cnt += 1
         else:
-            dr = (dr+1)%4
- 
-    print(f'#{test_case}')
-    for lst in arr:
-        print(*lst)
+            dr = (dr + 1) % 4  # 방향도 바꾸는데 인덱스는 3까지여서 4부터는 0으로 되돌아가게 하기
+    print(f"#{tc}")
+    for i in arr:
+        print(*i)
