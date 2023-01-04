9cdbd38be66d08611615fdee3ba214438a24c5d4
YoonDii <108647861+YoonDii@users.noreply.github.com>
2023-01-04 16:53:57 +0900

    [D2] Title: 달팽이 숫자, Time: 96 ms, Memory: 42,720 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
@@ -1,29 +1,23 @@
-t = int(input())
-# 우 하 좌 상
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
-
-for i in range(1, t + 1):
-    n = int(input())
-    data = [[0] * n for _ in range(n)]
-    # 초기 위치 및 회전 방향 정의
-    x, y = 0, 0
-    direction = 0
-
-    for j in range(1, n * n + 1):
-        data[x][y] = j
-        x += dx[direction]
-        y += dy[direction]
-
-        if not 0 <= x < n or not 0 <= y < n or data[x][y] != 0:
-            x -= dx[direction]
-            y -= dy[direction]
-            direction = (direction + 1) % 4
-            x += dx[direction]
-            y += dy[direction]
-
-    print("#%d" % i)
-    for j in range(n):
-        for k in range(n):
-            print(data[j][k], end=" ")
-        print()
+T = int(input())
+di = [0, 1, 0, -1]
+dj = [1, 0, -1, 0]
+for test_case in range(1, T + 1):
+    N = int(input())
+    arr = [[0] * N for _ in range(N)]
+ 
+    i, j, cnt, dr = 0, 0, 1, 0 # 초기화
+    arr[i][j] = cnt
+    cnt += 1
+ 
+    while cnt <= N*N:
+        ni, nj = i+di[dr], j+dj[dr]
+        if 0<=ni<N and 0<=nj<N and arr[ni][nj]==0:
+            i, j = ni, nj
+            arr[i][j]=cnt
+            cnt+=1
+        else:
+            dr = (dr+1)%4
+ 
+    print(f'#{test_case}')
+    for lst in arr:
+        print(*lst)
