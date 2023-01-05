a2a244c3330a74bd357c965ae057814ac19c270f
YoonDii <108647861+YoonDii@users.noreply.github.com>
2023-01-05 16:05:58 +0900

    [Silver IV] Title: 설탕 배달, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+count = 0
+
+while n >= 0:
+    if n % 5 == 0:
+        count += int(n // 5)
+        print(count)
+        break
+  
+    n -= 3
+    count += 1
+  
+else:
+    print(-1) 
