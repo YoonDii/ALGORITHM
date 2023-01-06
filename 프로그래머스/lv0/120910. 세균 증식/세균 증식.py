ed03433fb43bc8b28f8d2e9e807d6f076faeabe2
YoonDii <108647861+YoonDii@users.noreply.github.com>
2023-01-06 09:20:10 +0900

    [level 0] Title: 세균 증식, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120910. 세균 증식/세균 증식.py
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.py
@@ -1,7 +1,10 @@
-def solution(n, t):
-    result = n
-    for i in range(t):
-        result *= 2
-    return result
+# def solution(n, t):
+#     result = n
+#     for i in range(t):
+#         result *= 2
+#     return result
         
-       
+    
+def solution(n, t):
+    answer = n * (2 ** t)
+    return answer
