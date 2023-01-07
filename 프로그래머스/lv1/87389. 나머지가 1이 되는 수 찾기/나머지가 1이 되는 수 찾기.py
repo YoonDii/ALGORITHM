564ce412ae597093a240732af52873d864de83a2
YoonDii <108647861+YoonDii@users.noreply.github.com>
2023-01-07 22:34:59 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.py
+++ 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.py
@@ -3,6 +3,8 @@ def solution(n):
     for i in range(1,n+1):
         if n % i == 1 :
             num.append(i)
-    answer = min(num)
-    return answer
+            print(i)
+            return i
+    # answer = min(num)
+    # return answer
     
