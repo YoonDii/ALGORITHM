36d1c3f1db7e946e059668db917dfb1f076ccf26
YoonDii <108647861+YoonDii@users.noreply.github.com>
2023-01-14 16:20:16 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 3.14 ms, Memory: 11 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -0,0 +1,10 @@
+def solution(s):
+    answer = []
+    dic = dict()
+    for i in range(len(s)):
+        if s[i] not in dic:
+            answer.append(-1)
+        else:
+            answer.append(i - dic[s[i]])
+        dic[s[i]] = i
+    return answer
