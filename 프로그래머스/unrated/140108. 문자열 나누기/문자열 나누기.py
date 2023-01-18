9794d4176ef4987b008ccf7ac40552e3f8680b3a
YoonDii <108647861+YoonDii@users.noreply.github.com>
2023-01-18 12:54:59 +0900

    [unrated] Title: 문자열 나누기, Time: 1.44 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.py
@@ -0,0 +1,14 @@
+def solution(s):
+    answer = 0
+    cnt1 = 0
+    cnt2 = 0
+    
+    for i in s:
+        if cnt1 == cnt2 :
+            answer += 1
+            j = i
+        if j == i :
+            cnt1 += 1
+        else :
+            cnt2 += 1
+    return answer
