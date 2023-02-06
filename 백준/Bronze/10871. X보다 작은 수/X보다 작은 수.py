2e121d6841b9a4fd6c5bfdb2157f44b92bc94aa6
YoonDii <108647861+YoonDii@users.noreply.github.com>
2023-02-06 17:22:57 +0900

    [Bronze V] Title: X보다 작은 수, Time: 48 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -1,18 +1,8 @@
-#문제
-#정수 N개로 이루어진 수열 A와 정수 X가 주어진다. 이때, A에서 X보다 작은 수를 모두 출력하는 프로그램을 작성하시오.
-
-#입력
-#첫째 줄에 N과 X가 주어진다. (1 ≤ N, X ≤ 10,000)
-#둘째 줄에 수열 A를 이루는 정수 N개가 주어진다. 주어지는 정수는 모두 1보다 크거나 같고, 10,000보다 작거나 같은 정수이다.
-
-#출력
-#X보다 작은 수를 입력받은 순서대로 공백으로 구분해 출력한다. X보다 작은 수는 적어도 하나 존재한다.
-
-
 n, x = map(int,input().split())
-a = list(map(int,input().split())) #수열이니까 list로 변환해서 받기
-
-for i in range(n): # n개 중에
-    if a[i] < x:   # 수열a에 x보다 작은 수가 있으면
-        print(a[i], end = ' ') # 공백으로 구분해 출력하기
-
+nums = list(map(int,input().split()))
+# print(nums) [1, 10, 4, 9, 2, 3, 8, 5, 7, 6]
+num = []
+for i in nums:
+    if i < x :
+        num.append(i)
+print(*num)
