cb4698cca56f0910098c2e06b6867717e2dc13b1
YoonDii <108647861+YoonDii@users.noreply.github.com>
2023-02-06 17:40:38 +0900

    [Bronze II] Title: 음계, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.py
+++ 백준/Bronze/2920. 음계/음계.py
@@ -1,10 +1,10 @@
-t = list(map(int,input().split()))
+nums = list(map(int,input().split()))
 
-sing = [1,2,3,4,5,6,7,8]
+song = [1,2,3,4,5,6,7,8]
 
-if t == sing:
+if nums == song :
     print('ascending')
-elif t == sing[::-1]:
+elif nums == song[::-1]:
     print('descending')
 else:
     print('mixed')
