4ed96443951c489bf81dddb2b290eafa9094abc2
YoonDii <108647861+YoonDii@users.noreply.github.com>
2023-03-02 13:29:58 +0900

    [Bronze V] Title: 영수증, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.py
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -1,11 +1,14 @@
 x = int(input())
 n = int(input())
 
-for _ in range(n):
-    a, b = map(int,input().split())
-    #print(a,b)
-    x -= a * b
-if x == 0 :
-    print('Yes')
+ans = 0
+for i in range(n):
+    cash = list(map(int, input().split()))
+    money = cash[0] * cash[1]
+
+    if money:
+        ans += money
+if x == ans:
+    print("Yes")
 else:
-    print('No')
+    print("No")
