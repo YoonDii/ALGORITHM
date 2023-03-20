83fe4106a0f5a6446880584857c778d21ca50e80
YoonDii <108647861+YoonDii@users.noreply.github.com>
2023-03-20 09:43:45 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,21 @@
+def solution(num):
+    answer = 0
+    
+    if num == 1 :
+        return 0
+    
+    while True:
+        #num = num/2 if num % 2 == 0 else (num*3)+1
+        if num % 2 == 0:
+            num = num/2
+            answer += 1
+        else:
+            num = (num*3)+1
+            answer += 1
+        
+        if num == 1:
+            return answer
+        elif answer == 500 :
+            return -1
+        
+    return answer
