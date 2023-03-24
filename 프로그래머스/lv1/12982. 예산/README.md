8c8332ddb7e4b3e0954f9ca1c6719cb2fe9e8113
YoonDii <108647861+YoonDii@users.noreply.github.com>
2023-03-24 15:01:25 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/README.md
+++ 프로그래머스/lv1/12982. 예산/README.md
@@ -1,10 +1,10 @@
 # [level 1] 예산 - 12982 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12982#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12982) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.02 ms
+메모리: 10.2 MB, 시간: 0.01 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12982. 예산/예산.py
+++ 프로그래머스/lv1/12982. 예산/예산.py
@@ -1,13 +1,5 @@
 def solution(d, budget):
-    sd = sorted(d) # 먼저 정렬을 한다
-    answer = 0
-    for i in sd:
-        budget -= i # 예산에서 부서별지원금빼기
-        if budget < 0: #0보다 작아진다면 더하지 않고 멈추기
-            break
-        answer += 1 # 0보다 작지않다면 1씩더해주기
-    return answer # 예산이 모자르기 전에 지원한 부서의 개수
-    
-        
-        
-   
+    d.sort()
+    while budget < sum(d):
+        d.pop()
+    return len(d)
